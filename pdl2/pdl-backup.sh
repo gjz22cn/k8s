81664e696c953ccc82c9506e7b975577282485bd
gjz22cn <gjz22cn@hotmail.com>
2023-02-26 09:09:04 +0800

    Develop

---
+++ pdl2/pdl-backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+kubectl get pv -A -o yaml > pv.yaml
+kubectl get pvc -A -o yaml > pvc.yaml
+
+declare -a nss=("default" "dubbo" "flink" "hadoop" "hive" "kafka" "mysql" "presto" "redis" "xxl-job" "zookeeper")
+
+for ns in "${nss[@]}"
+do
+    if [ ! -d "$ns" ];then
+        mkdir $ns
+    fi
+    kubectl get pvc -n $ns -o yaml > $ns/$ns-pvc.yaml
+    kubectl get cm -n $ns -o yaml > $ns/$ns-cm.yaml
+    #kubectl get pods -n $ns -o yaml > $ns/$ns-pods.yaml
+    kubectl get deploy -n $ns -o yaml > $ns/$ns-deploy.yaml
+    if [ "$ns" == "hadoop" ];then
+        kubectl get statefulsets -n $ns -o yaml > $ns/$ns-statefulsets.yaml 
+    fi
+    kubectl get svc -n $ns -o yaml > $ns/$ns-svc.yaml
+done
